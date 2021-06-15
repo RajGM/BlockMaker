a0b4ce188f718e9f12efe6d55d9b22951c234932
RajGM <rajgmsocial19@gmail.com>
2021-06-15 13:11:52 +0530

    writing to file completed as well

--- buildBlock.js
+++ buildBlock.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 const data = require("./users.json");
 
 let tx_id_Map_withoutParent = new Map();
@@ -12,91 +14,85 @@ for (var i = 0; i < data.tx_id_array_withParent.length; i++) {
     tx_id_Map_withParent.set(currentTx_id.tx_id, { "fee": currentTx_id.fee, "weight": currentTx_id.weight, "isCompleted": false, "parents": currentTx_id.parents });
 }
 
+var printSeqID = [];
+
 var currentWeight = 0;
 var currentValue = 0;
 
 let tx_id_Map = new Map();
 var stopAll = false;
-for (var i = 0; i < data.tx_id_array_withoutParent.length; i++) {
 
-    if (data.tx_id_array_withoutParent[i].weight + currentWeight < data.metaData.maxWeight) {
+function calculateAll_withoutParnet() {
+    for (var i = 0; i < data.tx_id_array_withoutParent.length; i++) {
 
-        tx_id_Map.set(data.tx_id_array_withoutParent[i].tx_id, true);
-        tx_id_Map_withoutParent.set(data.tx_id_array_withoutParent[i].tx_id, { "fee": data.tx_id_array_withoutParent[i].fee, "weight": data.tx_id_array_withoutParent[i].weight, "isCompleted": true, "parents": data.tx_id_array_withoutParent[i].parents });
+        if (data.tx_id_array_withoutParent[i].weight + currentWeight < data.metaData.maxWeight && !tx_id_Map.has( data.tx_id_array_withoutParent[i].tx_id ) ) {
 
-        currentValue += data.tx_id_array_withoutParent[i].fee;
-        currentWeight += data.tx_id_array_withoutParent[i].weight;
+            tx_id_Map.set(data.tx_id_array_withoutParent[i].tx_id, true);
+            tx_id_Map_withoutParent.set(data.tx_id_array_withoutParent[i].tx_id, { "fee": data.tx_id_array_withoutParent[i].fee, "weight": data.tx_id_array_withoutParent[i].weight, "isCompleted": true, "parents": data.tx_id_array_withoutParent[i].parents });
 
-    } else {
-        stopAll = true;
-        break;
-    }
-
-}
-
-if (!stopAll) {
-
-
-
-
-}
-
-
-while (tx_id_Map_withParent.size >= 1) {
-
-    const iterator1 = tx_id_Map_withParent[Symbol.iterator]();
-
-    for (const item of iterator1) {
-
-        // console.log("item:",item);
-        // console.log("item[0]:",item[0]);
-        // console.log("item[1]:",item[1]);
-        // console.log("item[1].fee:",item[1].fee);
-        // console.log("item[1].weight:",item[1].weight);
-
-        if (item[1].weight + currentWeight < data.metaData.maxWeight) {
-
-            var parentsArray = item[1].parents;
-            for (var i = parentsArray.length - 1; i >= 0; i--) {
-                if (tx_id_Map.has(parentsArray[i])) {
-                    parentsArray.splice(i, 1);
-                }
-            }
-
-            parentArrayBreak:for (var i = 0; i < parentsArray.length; i++) {
-                if (calculate_Parent(parentsArray[i])) {
-
-                    if (i == parentsArray.length - 1) {
-
-                        if (item[1].weight + currentWeight <= data.metaData.maxWeight) {
-                            tx_id_Map.set(item[0], true);
-                            tx_id_Map_withParent.set(item[0], { "fee": item[1].fee, "weight": item[1].weight, "isCompleted": true, "parents": item[1].parents });
-                            currentValue += item[1].fee;
-                            currentWeight += item[1].weight;
-                        } else {
-                            tx_id_Map_withParent.delete(item[0]);
-                        }
-
-                    }
-
-                } else {
-                    tx_id_Map_withParent.delete(item[0]);
-                    break parentArrayBreak;
-                }
-
-            }
-
+            currentValue += data.tx_id_array_withoutParent[i].fee;
+            currentWeight += data.tx_id_array_withoutParent[i].weight;
+            printSeqID.push(data.tx_id_array_withoutParent[i].tx_id);
         } else {
-            tx_id_Map_withParent.delete(item[0]);
+            stopAll = true;
+            break;
         }
 
-
     }
 
 }
 
-console.log("TotalWeight:" + currentWeight);
-console.log("TotalFee:" + currentValue);
+function calculateAll_withParnet() {
+
+    while (tx_id_Map_withParent.size >= 1) {
+
+        const iterator1 = tx_id_Map_withParent[Symbol.iterator]();
+
+        for (const item of iterator1) {
+
+            if (item[1].weight + currentWeight < data.metaData.maxWeight) {
+
+                var parentsArray = item[1].parents;
+                for (var i = parentsArray.length - 1; i >= 0; i--) {
+                    if (tx_id_Map.has(parentsArray[i])) {
+                        parentsArray.splice(i, 1);
+                    }
+                }
+
+                parentArrayBreak: for (var i = 0; i < parentsArray.length; i++) {
+                    if (calculate_Parent(parentsArray[i])) {
+
+                        if (i == parentsArray.length - 1) {
+
+                            if (item[1].weight + currentWeight <= data.metaData.maxWeight) {
+                                tx_id_Map.set(item[0], true);
+                                tx_id_Map_withParent.set(item[0], { "fee": item[1].fee, "weight": item[1].weight, "isCompleted": true, "parents": item[1].parents });
+                                currentValue += item[1].fee;
+                                currentWeight += item[1].weight;
+                                printSeqID.push(item[0]);
+                            } else {
+                                tx_id_Map_withParent.delete(item[0]);
+                            }
+
+                        }
+
+                    } else {
+                        tx_id_Map_withParent.delete(item[0]);
+                        break parentArrayBreak;
+                    }
+
+                }
+
+            } else {
+                tx_id_Map_withParent.delete(item[0]);
+            }
+
+
+        }
+
+    }
+
+}
 
 function calculate_Parent(parentElement) {
     if (tx_id_Map.has(parentElement)) {
@@ -107,7 +103,7 @@ function calculate_Parent(parentElement) {
 
             tx_id_Map.set(parentElement, true);
             tx_id_Map_withoutParent.set(parentElement, { "fee": currentTx.fee, "weight": currentTx.fee, "isCompleted": true, "parents": currentTx.parents });
-
+            printSeqID.push(parentElement);
             currentValue += currentTx.fee;
             currentWeight += currentTx.weight;
 
@@ -118,7 +114,7 @@ function calculate_Parent(parentElement) {
 
     } else if (tx_id_Map_withParent.has(parentElement) && tx_id_Map_withParent.get(parentElement).isCompleted == false) {
         var moreParents = tx_id_Map_withParent.get(parentElement).parents;
-        
+
         for (var j = moreParents.length - 1; j >= 0; j--) {
             if (tx_id_Map.has(moreParents[j])) {
                 moreParents.splice(j, 1);
@@ -126,15 +122,16 @@ function calculate_Parent(parentElement) {
         }
 
         for (var i = 0; i < moreParents.length; i++) {
-                   
+
             if (calculate_Parent(moreParents[i])) {
 
                 if (i == moreParents.length - 1) {
-                        
+
                     if (tx_id_Map_withParent.get(parentElement).weight + currentWeight <= data.metaData.maxWeight) {
                         var currentTx = tx_id_Map_withParent.get(parentElement);
                         tx_id_Map.set(parentElement, true);
                         tx_id_Map_withParent.set(parentElement, { "fee": currentTx.fee, "weight": currentTx.weight, "isCompleted": true, "parents": currentTx.parents });
+                        printSeqID.push(parentElement);
                         currentValue += currentTx.fee;
                         currentWeight += currentTx.weight;
                     } else {
@@ -156,3 +153,23 @@ function calculate_Parent(parentElement) {
     }
 
 }
+
+function calculateAll() {
+  
+    calculateAll_withoutParnet();
+    if (!stopAll) {
+        calculateAll_withParnet();
+    }
+
+    fs.writeFile('block.txt', printSeqID.join('\n'), (err) => {
+      
+        // In case of a error throw err.
+        if (err) throw err;
+    })
+
+    console.log("TotalWeight:" + currentWeight);
+    console.log("TotalFee:" + currentValue);
+
+}
+
+calculateAll();
